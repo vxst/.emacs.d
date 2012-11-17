f441c5e0ecdf4f7591ea35ca8f2cf8962ab16a1c
Zeno Zeng <zenoes@qq.com>
2012-11-17 21:54:17 +0800

    模块化、ui自动黄金比例分割

--- init.el
+++ init.el
@@ -1,138 +1,39 @@
-;; Time-stamp: <2012-11-14 19:20:31 Zeno Zeng>
-(setq user-login-name "Zeno Zeng")
-;;;; load-path
+;; Time-stamp: <2012-11-17 21:45:07 Zeno Zeng>
 
-(add-to-list 'load-path "~/.emacs.d/modes")
 (add-to-list 'load-path "~/.emacs.d/init")
+(add-to-list 'load-path "~/.emacs.d/modes")
+(add-to-list 'load-path "~/.emacs.d/require")
 (add-to-list 'load-path "~/.emacs.d/modes/less-css-mode")
 (add-to-list 'load-path "~/.emacs.d/elpa/auto-complete-1.4")
 (add-to-list 'load-path "~/.emacs.d/elpa/popup-0.5")
-(add-to-list 'load-path "~/.emacs.d/init/emacs-w3m")
-(add-to-list 'load-path "~/.emacs.d/o-bloger")
-(add-to-list 'load-path "~/.emacs.d/elpa/org-20120903")
+(add-to-list 'load-path "~/.emacs.d/require/emacs-w3m")
+(add-to-list 'load-path "~/.emacs.d/require/o-bloger")
 (add-to-list 'load-path "~/.emacs.d/elpa/undo-tree-0.5.4")
+(add-to-list 'load-path "~/.emacs.d/elpa/org-20120903")
+
+
+(require 'init-ui)
+(require 'init-personal)
+(require 'init-functions)
+
+(require 'init-elpa)
+(require 'init-hs)
+(require 'init-auto-complete)
+(require 'init-backup)
+
+(require 'init-w3m)
+(require 'init-org)
+
+(require 'init-js)
+(require 'init-css)
+(require 'init-php)
 
 ;;;; UNDO TREE
 (require 'undo-tree)
 (global-undo-tree-mode)
-
 ;;;; END Undo Tree
 
-;; ;;;; 有道 API ::依赖xml.el,xml-parse.el
-;; ;;获取要翻译的词典  
-;; (defun get-current-word ()  
-;;   (interactive)  
-;;   (let ((begin (point-min)) (end (point-max)))  
-;;     (save-excursion        
-;;       (when (not mark-active)          
-;; 	(backward-word)  
-;; 	(mark-word))  
-;;       (setq begin (region-beginning)  
-;; 	    end (region-end)))  
-;;     (buffer-substring begin end)))  
 
-;; ;;抓取翻译结果  
-;; (defun get-translate-result (word)  
-;;   (shell-command-to-string (concat (format "curl 'http://fanyi.youdao.com/openapi.do?keyfrom=ZenoBlog&key=636489742&type=data&doctype=xml&version=1.1&q=%s' 2>/dev/null" word))))  
-
-;; ;;分析翻译结果,并返回显示字符串  
-;; (defun analytic-translate-result (translateresult)  
-;;   (let* ((root (with-temp-buffer (insert translateresult)  
-;; 				 (xml-parse-region (point-min) (point-max))))  
-;; 	 (youdao-fanyi (car root))  
-;; 	 (basic (car (xml-get-children youdao-fanyi 'basic)))  
-;; 	 (explains-ex (xml-get-children (car (xml-get-children basic 'explains)) 'ex))  
-;; 	 (phonetic-texts (car (xml-node-children (car (xml-get-children basic 'phonetic)))))   
-;; 	 (explains-texts (concat phonetic-texts "\n")))  
-;;     (loop for ex in explains-ex  
-;; 	  do (setq explains-texts (concat explains-texts  (car (xml-node-children ex)) "\n")))  
-;;     explains-texts))  
-
-;; ;;主函数  
-;; (defun lookup-word ()  
-;;   (interactive)  
-;;   (let* ((word (get-current-word))  
-;; 	 (translate-result (get-translate-result word))  
-;; 	 (explains-texts (analytic-translate-result translate-result)))  
-;;     (tooltip-show explains-texts t)))  
-;; ;;绑定到Ctrl-c f组合键上  
-;; (global-set-key (kbd "C-c f") 'lookup-word)  
-;; ;;;; END 有道 API
-
-;;;; Frames and Graphical Displays
-
-;;; UI
-;; 关闭Emacs启动时提示信息
-;; (setq inhibit-startup-message t)
-;; 各种bar
-(set-scroll-bar-mode nil)
-(blink-cursor-mode -1)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-;; (menu-bar-mode (quote toggle))
-;;; Modeline
-;; 电池
-(setq battery-mode-line-format "[%b%p%%]")
-(display-battery-mode 1)
-;; 时间
-(setq display-time-24hr-format t)
-(setq display-time-day-and-date t)
-(setq display-time-format "[%m-%d %a %H:%M]")
-;; 不要显示load average
-(setq display-time-load-average-threshold 100)
-(display-time)
-(setq display-time-24hr-format t)
-(setq column-number-mode t)
-(setq line-number-mode t)
-;; 标题栏 
-(setq frame-title-format "%f")
-;; bu显示匹配括号
-(show-paren-mode nil)
-;; 匹配括号时不跳至另一括号
-;;(setq show-paren-style 'parentheses)
-;; "yes or no" =>  "y or n"
-(fset 'yes-or-no-p 'y-or-n-p)
-;; 高亮显示mark
-(transient-mark-mode t)
-;; 开启语法高亮。
-(global-font-lock-mode 1)
-;; 开启 ido mode
-(ido-mode nil)
-(global-set-key "\C-xk" (lambda ()
-                          (interactive)
-			  (kill-buffer (buffer-name))))
-;; highlight-parentheses
-(require 'highlight-parentheses)
-(define-globalized-minor-mode global-highlight-parentheses-mode
-  highlight-parentheses-mode
-  (lambda ()
-    (highlight-parentheses-mode t)))
-(global-highlight-parentheses-mode t)
-
-;; 光标靠近鼠标指针时，让鼠标指针自动让开，别挡住视线。
-(mouse-avoidance-mode 'animate)
-
-;; MY YANK
-;; 例子：
-;; C-u 100 C-y
-;; 复制一百次
-(defun my-yank (arg)
-  "Yank ARG times."
-  (interactive "p")
-  (dotimes (var arg)
-    (yank)))
-
-;; 设置个人信息
-(setq user-full-name "Zeno Zeng")
-(setq user-mail-address "zenoes@qq.com")
-
-;;;; PHP Mode
-(setq php-manual-path "~/doc/php-manual/")
-;;;; END PHP Mode
-
-
-;;;; Org Mode
-(setq org-log-done t)
 
 ;;obloger
 (require 'o-bloger)
@@ -143,26 +44,14 @@
 (setq-default kill-whole-line t)
 
 ;; 自动更新time-stamp
-(setq time-stamp-format "%:y-%02m-%02d %02H:%02M:%02S %u")
+(setq time-stamp-format "%:y-%02m-%02d %02H:%02M:%02S %U")
 (add-hook 'before-save-hook 'time-stamp)
 ;; 不要使用tab进行缩进
 (setq indent-tabs-mode nil)
 
-;; 一键缩进
-(defun indent-buffer ()
-  "Indent the current buffer"
-  (interactive)
-  
-  ;; 尝试直接缩进
-  (ignore-errors
-    (indent-region (point-min) (point-max)))
-  
-  ;; 尝试逐行缩进
-  (save-excursion
-    (goto-char (point-min))
-    (while (search-forward "\n" nil t)
-      (ignore-errors
-        (indent-for-tab-command)))))
+
+
+
 
 ;;设定删除保存记录为200，可以方便以后无限恢复
 (setq kill-ring-max 200)
@@ -170,12 +59,6 @@
 ;;允许emacs和外部其他程式的粘贴
 (setq x-select-enable-clipboard t)
 
-;;; w3m
-(require 'w3m)
-;;设置w3m为emacs的默认浏览器
-(setq browse-url-browser-function 'w3m-browse-url)
-(autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
-(global-set-key "\C-xm" 'browse-url-at-point)
 
 ;;;; Modes
 
@@ -188,56 +71,7 @@
 
 ;; Type `M-x gimp-help' for help.
 
-;; 代码折叠
 
-(add-hook 'c-mode-common-hook   'my-hs)
-(add-hook 'emacs-lisp-mode-hook 'my-hs)
-(add-hook 'java-mode-hook       'my-hs)
-(add-hook 'js2-mode-hook        'my-hs)
-(add-hook 'perl-mode-hook       'my-hs)
-(add-hook 'sh-mode-hook         'my-hs)
-(add-hook 'scheme-mode-hook     'my-hs)
-(add-hook 'css-mode-hook        'my-hs)
-(add-hook 'less-mode-hook       'my-hs)
-(add-hook 'php-mode-hook     (lambda ()
-                               (my-hs)
-                               (hs-hide-level 2)))
-
-;; 导出的时候就不必隐藏了。
-(add-hook 'htmlize-before-hook   'hs-show-all)
-
-(setq hs-hide-comments-when-hiding-all nil)
-
-(defun my-hs ()
-  "My hideshow mode."
-  (hs-minor-mode)
-  (hs-hide-all))
-
-;; AC
-(require 'auto-complete)
-(require 'auto-complete-config)
-(set-default 'ac-sources
-             '(ac-source-dictionary
-               ac-source-words-in-buffer
-               ac-source-words-in-same-mode-buffers))
-
-(defun my-ac-mode ()
-    (auto-complete-mode 1)
-    (ac-flyspell-workaround))
-
-(dolist (hook '(
-                js2-mode-hook
-		html-mode-hook
-		less-css-mode-hook
-		))
-  (add-hook hook 'my-ac-mode))
-
-
-;; PHP
-(autoload 'php-mode "php-mode.el"
-  "Major mode for editing PHP files" t)
-(setq auto-mode-alist
-      (cons '("\\.php" . php-mode) auto-mode-alist))
 
 ;; firefox
 (defun ff()
@@ -253,96 +87,13 @@
 (setq auto-mode-alist
       (cons '("\\.md" . markdown-mode) auto-mode-alist))
 
-;; Less
-(autoload 'less-css-mode "less-css-mode-rm-flymake.el"
-  "Major mode for editing less-css files" t)
-(setq auto-mode-alist
-      (cons '("\\.less" . less-css-mode) auto-mode-alist))
 ;; Html mode
 (setq auto-mode-alist
       (cons '("\\.html" . html-mode) auto-mode-alist))
-;; Rainbow
-(dolist (hook '(css-mode-hook
-		html-mode-hook
-		python-mode-hook
-		less-css-mode-hook
-		lisp-mode-hook))
-  (add-hook hook (lambda () (rainbow-mode 1))))
 ;;;; END Modes
 
 
-;; 备份文档路径
-(setq backup-directory-alist (quote (("." . "~/.backups"))))
-;;生成临时文件
-(setq-default make-backup-file t)
-(setq make-backup-file t)
-(setq make-backup-files t)
-(setq delete-old-versions t)
-;;自动存盘
-(setq auto-save-mode t)
-;;击键100次自动保存
-(setq auto-save-interval 100)
 
-
-;;;; END Backup
-
-;;;; ORG MODE
-(setq org-agenda-files (list  "~/org/mobileorg/index.org" "~/org/life.org"))
-(setq org-mobile-directory "~/org/mobileorg")
-(setq org-hide-leading-stars t)
-(define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done 'time)
-
-(require 'org-publish)
-(setq org-publish-project-alist
-      '(("orgfiles"
-	 :base-directory "~/org/notes/publish/"
-	 :base-extension "org"
-	 :publishing-directory "/ftp:zvame@216.18.217.25#21:/domains/zenoes.com/public_html/notes/"
-	 ;;	 :publishing-directory "~/public_html/notes/"
-	 :publishing-function org-publish-org-to-html
-	 :headline-levels 6
-	 :section-numbers nil
-	 :table-of-contents nil
-	 :style "<link rel=\"stylesheet\" href=\"style/style.css\" type=\"text/css\"/>"
-	 :style-extra "
-<script src=\"http://lib.sinaapp.com/js/jquery/1.7.2/jquery.min.js\"></script>
-<script src=\"style/functions.js\"></script>"
-	 :style-include-default nil
-	 :style-include-scripts nil
-	 :creator-info nil
-	 :html-preamble t)
-        
-        
-	("other"
-	 :base-directory "~/org/notes/"
-	 :base-extension "css\\|js"
-	 :publishing-directory "/ftp:zvame@216.18.217.25#21:/domains/zenoes.com/public_html/notes/style/"
-	 :publishing-function org-publish-attachment)
-        
-	("np" :components ("notepublish" "other"))
-        
-	("notes" :components ("orgfiles" "other"))))
-
-
-;; Capture
-(setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/org/tasks.org" "任务")
-	 "* TODO %?\n")
-	("e" "Exam" entry (file+headline "~/org/study.org" "考试")
-	 "* TODO %?\n")
-	("h" "Homework" entry (file+headline "~/org/study.org" "作业")
-	 "* TODO %?\n")
-	("e" "Emacs Notes" entry (file+headline "~/org/notes/emacs.org" "Notes")
-	 "* %?\n %i\n %a")
-	("p" "Emacs Notes" entry (file+headline "~/org/notes/php.org" "capture")
-	 "* %?\n %i\n %a")
-	("l" "Emacs Lisp Notes" entry (file+headline "~/org/notes/lisp.org" "Notes")
-	 "* %?\n %i\n %a")
-	("j" "Journal" entry (file+datetree "~/org/journal.org")
-	 "* %?")))
-
-;;;; END Orgmode
 
 ;;;;  文件加密
 
@@ -427,255 +178,35 @@
 
 ;; C-x C-j open the directory of current buffer
 (global-set-key (kbd "C-x C-j")
-		(lambda ()
-		  (interactive)
-		  (if (buffer-file-name)
-		      (dired default-directory))))
+                (lambda ()
+                  (interactive)
+                  (if (buffer-file-name)
+                      (dired default-directory))))
 
 ;; 这里重启两次是为了 防止toggle类的fun出现问题
 (global-set-key (kbd "H-i") '(lambda ()
-			       (interactive)
-			       (indent-buffer)))
+                               (interactive)
+                               (indent-buffer)))
 (global-set-key (kbd "H-x") 'execute-extended-command)
 (global-set-key (kbd "H-r") 'replace-regexp)
 (global-set-key [f2] 'hs-hide-all)
 (global-set-key (kbd "H-e") '(lambda ()
-			       (interactive)
-			       (eval-buffer)))
+                               (interactive)
+                               (eval-buffer)))
 (global-set-key [f6] 'toggle-truncate-lines)
 
 ;; 全屏
 (global-set-key [f11] '(lambda ()
-			 (interactive)
-			 (x-send-client-message
-			  nil 0 nil "_NET_WM_STATE" 32
-			  '(2 "_NET_WM_STATE_FULLSCREEN" 0))))
+                         (interactive)
+                         (x-send-client-message
+                          nil 0 nil "_NET_WM_STATE" 32
+                          '(2 "_NET_WM_STATE_FULLSCREEN" 0))))
 
 (global-set-key [f12] 'archive-region)
 
-(autoload 'espresso-mode "espresso")
-(defun my-js2-indent-function ()
-  (interactive)
-  (save-restriction
-    (widen)
-    (let* ((inhibit-point-motion-hooks t)
-	   (parse-status (save-excursion (syntax-ppss (point-at-bol))))
-	   (offset (- (current-column) (current-indentation)))
-	   (indentation (espresso--proper-indentation parse-status))
-	   node)
-      
-      (save-excursion
-	
-	;; I like to indent case and labels to half of the tab width
-	(back-to-indentation)
-	(if (looking-at "case\\s-")
-	    (setq indentation (+ indentation (/ espresso-indent-level 2))))
-	
-	;; consecutive declarations in a var statement are nice if
-	;; properly aligned, i.e:
-	;;
-	;; var foo = "bar",
-	;;     bar = "foo";
-	(setq node (js2-node-at-point))
-	(when (and node
-		   (= js2-NAME (js2-node-type node))
-		   (= js2-VAR (js2-node-type (js2-node-parent node))))
-	  (setq indentation (+ 4 indentation))))
-      
-      (indent-line-to indentation)
-      (when (> offset 0) (forward-char offset)))))
-(defun my-indent-sexp ()
-  (interactive)
-  (save-restriction
-    (save-excursion
-      (widen)
-      (let* ((inhibit-point-motion-hooks t)
-	     (parse-status (syntax-ppss (point)))
-	     (beg (nth 1 parse-status))
-	     (end-marker (make-marker))
-	     (end (progn (goto-char beg) (forward-list) (point)))
-	     (ovl (make-overlay beg end)))
-	(set-marker end-marker end)
-	(overlay-put ovl 'face 'highlight)
-	(goto-char beg)
-	(while (< (point) (marker-position end-marker))
-	  ;; don't reindent blank lines so we don't set the "buffer
-	  ;; modified" property for nothing
-	  (beginning-of-line)
-	  (unless (looking-at "\\s-*$")
-	    (indent-according-to-mode))
-	  (forward-line))
-	(run-with-timer 0.5 nil '(lambda(ovl)
-				   (delete-overlay ovl)) ovl)))))
-(defun my-js2-mode-hook ()
-  (require 'espresso)
-  (setq espresso-indent-level 4
-	indent-tabs-mode nil
-	c-basic-offset 4)
-  (c-toggle-auto-state 0)
-  (c-toggle-hungry-state 1)
-  (set (make-local-variable 'indent-line-function) 'my-js2-indent-function)
-  (define-key js2-mode-map [(meta control |)] 'cperl-lineup)
-  (define-key js2-mode-map [(meta control \;)] 
-    '(lambda()
-       (interactive)
-       (insert "/* -----[ ")
-       (save-excursion
-	 (insert " ]----- */"))
-       ))
-  (define-key js2-mode-map [(return)] 'newline-and-indent)
-  (define-key js2-mode-map [(backspace)] 'c-electric-backspace)
-  (define-key js2-mode-map [(control d)] 'c-electric-delete-forward)
-  (define-key js2-mode-map [(control meta q)] 'my-indent-sexp)
-  (if (featurep 'js2-highlight-vars)
-      (js2-highlight-vars-mode))
-  (message "My JS2 hook"))
 
-(add-hook 'js2-mode-hook 'my-js2-mode-hook)
-
-;; Emacs 软件源 (stable releases)
-(require 'package)
-(add-to-list 'package-archives 
-             '("marmalade" .
-               "http://marmalade-repo.org/packages/"))
-(package-initialize)			    
-
-;; GTD
-(defun zeno-comm (date &optional show-all)
-  "Common function for summary of agenda files"
-  (let ((count-failed-sum 0)
-        (count-done-sum 0)
-        (count-postponed-sum 0))
-    
-    (if show-all
-        (setq output (concat output "* " date "<>\n")))
-    
-    (dolist (file org-agenda-files)
-      (find-file file)
-      (goto-char (point-min))
-      
-      (setq count-failed (count-matches (format "State \"FAILED\".*%s" date))
-            count-done (count-matches (format "State \"DONE\".*%s" date))
-            count-postponed (count-matches (format "State \"POSTPONED\".*%s" date))
-            count-sum (+ count-failed count-done count-postponed)
-            count-failed-sum (+ count-failed count-failed-sum)
-            count-postponed-sum (+ count-postponed count-postponed-sum)
-            count-done-sum (+ count-done count-done-sum))
-      
-      (when show-all
-        (setq new (format "** %s\n*** DONE : %d\n*** FAILED : %d\n*** 任务总数 : %d\n*** 完成率 : %f\n*** 延滞率 : %f\n\n"
-                          (buffer-name)
-                          count-done
-                          count-failed
-                          count-sum
-                          (if (= count-sum 0)
-                              0
-                            (/ count-done (float count-sum)))
-                          (if (= count-sum 0)
-                              0
-                            (/ count-postponed (float count-sum))))
-              output (concat output new))))
-    
-    (setq count-sum-sum (+ count-failed-sum count-postponed-sum count-done-sum))
-    
-    (when show-all
-      (setq new (format "** %s\n*** DONE : %d\n*** FAILED : %d\n*** 任务总数 : %d\n*** 完成率 : %f\n*** 延滞率 : %f\n\n"
-                        "Sum"
-                        count-done-sum
-                        count-failed-sum
-                        count-sum-sum
-                        (if (= 0 count-sum-sum)
-                            0
-                          (/ count-done-sum (float count-sum-sum)))
-                        (if (= count-sum-sum 0)
-                            0
-                          (/ count-postponed-sum (float count-sum-sum))))
-            output (concat output new))
-      
-      (setq output (replace-regexp-in-string
-                    (concat date "<>") (format "%s <%f>"
-                                               date
-                                               (if (= 0 count-sum-sum)
-                                                   0
-                                                 (/ count-done-sum (float count-sum-sum)))) output)))
-    
-    ;; 返回percent
-    (if (= 0 count-sum-sum)
-        0
-      (/ count-done-sum (float count-sum-sum)))))
-(defun zeno()
-  "Summary for my agenda files"
-  (interactive)
-  (let (output)
-    (zeno-comm (format-time-string "%Y") t)
-    (zeno-comm (format-time-string "%Y-%m") t)
-    (zeno-comm (format-time-string "%Y-%m-%d") t)
-    
-    
-    (setq var (- (zeno-comm (format-time-string "%Y-%m-%d"))
-                 (zeno-comm (format-time-string "%Y-%m-%d" (time-add (current-time) (days-to-time -1))) nil))
-          output (replace-regexp-in-string
-                  (format-time-string "%Y-%m-%d")
-                  (format "%s [%f]" (format-time-string "%Y-%m-%d") var)
-                  output))
-    
-    (setq var (- (zeno-comm (format-time-string "%Y-%m"))
-                 (zeno-comm (format-time-string "%Y-%m" (time-add (current-time) (days-to-time (- -1 (string-to-int (format-time-string "%d")))))) nil))
-          output (replace-regexp-in-string
-                  (format-time-string "%Y-%m <")
-                  (format "%s [%f] <" (format-time-string "%Y-%m") var)
-                  output))
-    
-    
-    (dotimes (i 6)
-      (zeno-comm (format-time-string
-                  "%Y-%m-%d"
-                  (time-add (current-time) (days-to-time (- -1 i)))) t))
-    
-    (find-file (format-time-string "~/.archive/zeno/%Y-%m-%d.org" (current-time)))
-    (erase-buffer)
-    (insert output)
-    (save-buffer)
-    (goto-char (point-min))
-    (org-global-cycle t)
-    (next-line)
-    (next-line)
-    (show-subtree)
-    (delete-other-windows)
-    (org-agenda-list)))
-(setq org-todo-keywords
-      '((sequence "TODO(t)" "WAIT(w@/!)" "POSTPONED(p!)""|" "DONE(d!)" "FAILED(f!)" "CANCELED(c!)")))
-
-;; ;;;; English Writing
-;; (require 'english-writing-mode)
-
-;; ;; 自动拼写检查
-;; (setq-default ispell-program-name "aspell")     ;用aspell替换ispell, 更加智能
-;; (setq-default ispell-extra-args '("--reverse")) ;修复aspell与ispell冲突的bug
-;; (setq ispell-dictionary "english")      ;设置英文词典
-;; (add-hook 'english-writing-mode-hook 'flyspell-mode)
-
-;; ;; 单词自动补全
-;; (defun my-writting-auto-complete ()
-;;   (when (eq major-mode 'english-writing-mode)
-;;     (let* ((beg
-;; 	    (save-excursion
-;; 	      (backward-word)
-;; 	      (point)))
-;; 	   (dabbrev-search-these-buffers-only
-;; 	    (list "words")))
-;;       (if (> (- (point) beg) 3)
-;; 	  (dabbrev-completion 16)))))
-;; (add-hook 'english-writing-mode-hook 'auto-complete-mode)
-;; (add-hook 'post-self-insert-hook 'my-writting-auto-complete)
-;; (find-file-noselect "/usr/share/dict/words")
-
-
-
-;;;; END ENGLISH 
 
 ;; 填入大中小括号，双单引号的匹配
 ;; 放最后，不然导致缩进错误
 (setq skeleton-pair t)
 (setq skeleton-pair-alist '((?\" _ "\"" >)(?\' _ "\'" >)(?《 _"》">)(?（ _"）">)(?\( _ ")" >)(?\[ _ "]" >)(?\{ _ "}" >)))
-			    
