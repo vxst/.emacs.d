aac95a3495fa336379c37fe6e6e4ad7857c0f9bf
Zeno Zeng <zenoes@qq.com>
2012-11-17 22:23:12 +0800

    correct a mistake

--- init/init-php.el
+++ init/init-php.el
@@ -3,6 +3,12 @@
 (setq auto-mode-alist
       (cons '("\\.php" . php-mode) auto-mode-alist))
 
-(setq php-manual-path "~/doc/php-manual/")
+;; 添加类似elisp的函数查询，依赖php-extras
+(add-hook 'php-mode-hook '(lambda ()
+ 			    (local-set-key (kbd "H-f") 'php-extras-eldoc-documentation-function)))
 
+;; The function php-extras-generate-eldoc will download the PHP manual from php.net and extract the function definitions (slow) and store them in a hash table on disk for you. If you install php-extras as an ELPA package the hash table is already generated for you.
+
+(defun my-php-mode-stuff ()
+  (local-set-key (kbd "<f1>") 'my-php-symbol-lookup))
 (provide 'init-php)
