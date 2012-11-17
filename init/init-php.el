896276ffb4cd3f3065ab685c1d60b1e9bb7d29f8
Zeno Zeng <zenoes@qq.com>
2012-11-17 22:34:59 +0800

    eldoc-mode in php

--- init/init-php.el
+++ init/init-php.el
@@ -5,9 +5,7 @@
 
 ;; 添加类似elisp的函数查询，依赖php-extras
 (add-hook 'php-mode-hook '(lambda ()
- 			    (local-set-key (kbd "H-f") 'php-extras-eldoc-documentation-function)))
-
-;; The function php-extras-generate-eldoc will download the PHP manual from php.net and extract the function definitions (slow) and store them in a hash table on disk for you. If you install php-extras as an ELPA package the hash table is already generated for you.
+			    (eldoc-mode)))
 
 (defun my-php-mode-stuff ()
   (local-set-key (kbd "<f1>") 'my-php-symbol-lookup))
