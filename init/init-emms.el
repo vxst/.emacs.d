4e4e3e965ab09b7a057f6c2ea0f01b510d7d5adc
Zeno Zeng <zenoes@qq.com>
2012-12-09 12:49:27 +0800

    update init-emms

--- init/init-emms.el
+++ init/init-emms.el
@@ -8,6 +8,13 @@
 (emms-playing-time-disable-display)
 (emms-lyrics-disable)
 
+;; 打开时自动加载，完毕后暂停
+(emms-add-directory-tree "~/音乐/")
+(emms-shuffle)
+(emms-start)
+(emms-pause)
+
+
 ;; global key-map
 ;; all global keys prefix is C-c e
 ;; compatible with emms-playlist mode keybindings
@@ -18,15 +25,13 @@
 				  (interactive)
 				  (emms-score-down-playing)
 				  (emms-next)))
-(global-set-key (kbd "C-c e p") 'emms-previous)
+(global-set-key (kbd "C-c e p") (lambda ()
+				  (interactive)
+				  (emms-score-up-playing)
+				  (emms-previous)))
 (global-set-key (kbd "C-c e f") 'emms-show)
 ;; these keys maps were derivations of above keybindings
 (global-set-key (kbd "C-c e t") 'emms-play-directory-tree)
-(global-set-key (kbd "C-c e e") (lambda ()
-				  (interactive)
-				  (emms-add-directory-tree "~/音乐/")
-				  (emms-shuffle)
-				  (emms-start)))
 (global-set-key (kbd "C-c e l") 'emms-play-playlist)
 (global-set-key (kbd "C-c e u") 'emms-score-up-playing)
 (global-set-key (kbd "C-c e d") 'emms-score-down-playing)
