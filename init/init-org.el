7f6f596f4228de0da08aee2451d12f21d5a9471e
Zeno Zeng <zenoes@qq.com>
2012-12-11 14:45:09 +0800

    update org mode config

--- init/init-org.el
+++ init/init-org.el
@@ -1,16 +1,25 @@
-(setq org-agenda-files (list "~/org/new-gtd.org"))
+(setq org-agenda-files (list "~/org/new-gtd.org" "~/org/journal.org"))
 (setq org-mobile-directory "~/org/mobileorg")
 (setq org-hide-leading-stars t)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done 'time)
 
+(setq org-todo-keywords
+      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!/!)")
+              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)")
+	      (sequence "|"  "FAILED(f)" ))))
+(setq org-todo-keyword-faces
+      (quote (("TODO" :foreground "orange red" :weight bold)
+              ("NEXT" :foreground "deepskyblue" :weight bold)
+              ("DONE" :foreground "LawnGreen" :weight bold)
+              ("WAITING" :foreground "tomato" :weight bold)
+              ("HOLD" :foreground "deep pink" :weight bold)
+              ("CANCELLED" :foreground "gray" :weight bold)
+	      ("FAILED" :foreground "dark grey" :weight bold))))
+
 ;; Capture
 (setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/org/tasks.org" "任务")
-	 "* TODO %?\n")
-	("e" "Exam" entry (file+headline "~/org/study.org" "考试")
-	 "* TODO %?\n")
-	("h" "Homework" entry (file+headline "~/org/study.org" "作业")
+      '(("t" "Todo" entry (file+headline "~/org/new-gtd.org" "Inbox")
 	 "* TODO %?\n")
 	("e" "Emacs Notes" entry (file+headline "~/org/notes/emacs.org" "Notes")
 	 "* %?\n %i\n %a")
@@ -21,9 +30,10 @@
 	("j" "Journal" entry (file+datetree "~/org/journal.org")
 	 "* %?")))
 
+
+(require 'org-my-exp)
 (add-hook 'org-mode-hook (lambda ()
 			   (local-unset-key (kbd "H-e"))
 			   (local-set-key (kbd "H-e") 'org-my-exp)))
 
-(require 'org-my-exp)
 (provide 'init-org)
