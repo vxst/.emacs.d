009f58f8195ee62caa430a4b081239b5e6d8a2c9
Zeno Zeng <zenoes@qq.com>
2012-12-25 18:40:28 +0800

    Tired of seeing stale dired buffers?

--- init/init-dired.el
+++ init/init-dired.el
@@ -1,4 +1,12 @@
 (defalias 'mkdir 'make-directory)
+
+;;; Tired of seeing stale dired buffers?
+;; Auto refresh buffers
+(global-auto-revert-mode 1)
+;; Also auto refresh dired, but be quiet about it
+(setq global-auto-revert-non-file-buffers t)
+(setq auto-revert-verbose nil)
+
 (require 'dired-filetype-face)
 (provide 'init-dired)
 ;;; init-dired.el ends here
