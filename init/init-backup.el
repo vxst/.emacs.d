844a8e6720f7d30161d151bc5cadfed47bcb6bb6
Zeno Zeng <zenoes@qq.com>
2012-12-25 22:55:26 +0800

    update backup dir

--- init/init-backup.el
+++ init/init-backup.el
@@ -2,7 +2,7 @@
 ;; Write backup files to own directory
 (setq backup-directory-alist
       `(("." . ,(expand-file-name
-                 (concat user-emacs-directory "backups")))))
+                 "~/.backups"))))
 
 ;; Files covered by version control don't get backups.
 (setq vc-make-backup-files nil)
