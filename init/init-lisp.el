1618c10e22a185a9d5e7265b6c564966c6061be3
Zeno Zeng <zenoes@qq.com>
2013-02-04 15:38:28 +0800

    use guile as scheme program instead of mit-scheme

--- init/init-lisp.el
+++ init/init-lisp.el
@@ -1,2 +1,28 @@
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
+
+(global-set-key (kbd "s-e") 'my-eval-buffer)
+(defun my-eval-buffer ()
+  (interactive)
+  (if (eq major-mode 'scheme-mode)
+      (scheme-send-region (point-min) (point-max))
+    (eval-buffer)))
+
+
+(global-set-key [f8] 'my-run-scheme)
+(defun my-run-scheme ()
+  (interactive)
+  (if (get-buffer "*scheme*")
+      (switch-to-buffer "*scheme*")
+    (progn
+      (run-scheme "mit-scheme")
+      (switch-to-buffer (find-file-noselect "~/git/scheme/math.scm"))
+      (scheme-send-region (point-min) (point-max))
+      (switch-to-buffer "*scheme*"))))
+
+;; scheme
+;; apt-get install guile-2.0
+;; C-c C-r runs the command scheme-send-region
+(setq scheme-program-name "guile")
+
+
 (provide 'init-lisp)
