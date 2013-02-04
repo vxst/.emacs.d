2de25aa75b8ab26e82cb4badf87db8d536094ab7
Zeno Zeng <zenoes@qq.com>
2013-02-04 15:39:59 +0800

    *** empty log message ***

--- init/init-lisp.el
+++ init/init-lisp.el
@@ -14,7 +14,7 @@
   (if (get-buffer "*scheme*")
       (switch-to-buffer "*scheme*")
     (progn
-      (run-scheme "mit-scheme")
+      (run-scheme "guile")
       (switch-to-buffer (find-file-noselect "~/git/scheme/math.scm"))
       (scheme-send-region (point-min) (point-max))
       (switch-to-buffer "*scheme*"))))
