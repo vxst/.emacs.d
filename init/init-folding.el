a1d346b6eb789149ab841a6e6e872b937082e215
Zeno Zeng <zenoes@qq.com>
2013-03-02 21:31:31 +0800

    add init-folding

---
+++ init/init-folding.el
@@ -0,0 +1,13 @@
+;; 代码折叠
+
+;; 导出的时候就不必隐藏了。
+;; (add-hook 'htmlize-before-hook   'hs-show-all)
+
+;; 依照缩进来折叠代码
+(define-key global-map (kbd "C-'") 'yafolding)
+
+(add-hook 'emacs-lisp-mode-hook
+	  (lambda ()
+	    (yafolding-hide-all 2)))
+
+(provide 'init-folding)
