aabdad1d8a92b19f7f897496514e17235f0b2569
Zeno Zeng <zenoes@qq.com>
2013-04-18 17:31:56 +0800

    update .emacs.d

--- init/init-folding.el
+++ init/init-folding.el
@@ -6,12 +6,20 @@
 
 ;; 依照缩进来折叠代码
 (define-key global-map [C-return] 'yafolding)
-(define-key global-map (kbd "C-c C-f") 'yafolding-toggle-all)
+;;(define-key global-map (kbd "C-c C-f") 'yafolding-toggle-all)
+(define-key global-map (kbd "C-c C-f") 'yafolding-toggle-all-by-current-level)
+
+(add-hook 'indent-buffer-before-hook
+	  (lambda ()
+	    (yafolding-temp-toggle nil)))
+(add-hook 'indent-buffer-after-hook
+	  (lambda ()
+	    (yafolding-temp-toggle t)))
 
 (add-hook 'js3-mode-hook
 	  (lambda ()
-	    (local-set-key [C-return] 'js3-mode-toggle-element)
-	    (local-set-key (kbd "C-c C-f") 'js3-mode-toggle-hide-functions)
+;;	    (local-set-key [C-return] 'js3-mode-toggle-element)
+	    (local-set-key (kbd "C-c C-f") 'yafolding-toggle-all-by-current-level)
 	    ))
 
 (provide 'init-folding)
