17b08310e2cd705c21513fee4866616e957ebd69
Zeno Zeng <zenoes@qq.com>
2013-04-23 16:54:31 +0800

    *** empty log message ***

--- init/init-etc.el
+++ init/init-etc.el
@@ -52,22 +52,8 @@
 (global-set-key [f2] 'hs-hide-all)
 (global-set-key [f6] 'toggle-truncate-lines)
 
-;; 全屏
-;; (global-set-key [f11] '(lambda ()
-;; 			 (interactive)
-;; 			 (x-send-client-message
-;; 			  nil 0 nil "_NET_WM_STATE" 32
-;; 			  '(2 "_NET_WM_STATE_FULLSCREEN" 0))))
-;; (x-send-client-message
-;;  nil 0 nil "_NET_WM_STATE" 32
-;;  '(2 "_NET_WM_STATE_FULLSCREEN" 0))
-
-;;按f11让Emacs进入全屏显示
-;;参考： http://www.emacswiki.org/cgi-bin/wiki/FullScreen
-(defun fullscreen ()
-  (interactive)
-  (set-frame-parameter nil 'fullscreen
-		       (if (frame-parameter nil 'fullscreen) nil 'fullboth)))
+;;; 全屏
+;; F11全屏，原先向X发送message的方法现在似乎失效了，直接在gnome的keyboard shortcuts绑定f11到全屏就好
 
 ;;;; English Writing
 (require 'english-writing-mode)
