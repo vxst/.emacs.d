0bb078c928220933f2b7e923304d6b8c868bf5c1
Zeno Zeng <zenoes@qq.com>
2013-08-12 14:34:08 +0800

    some config for dired

--- init/init-dired.el
+++ init/init-dired.el
@@ -1,4 +1,40 @@
+;;; alias
+
 (defalias 'mkdir 'make-directory)
+(defalias 'mnt 'media)
+
+(defun media()
+  (interactive)
+  (dired "/media/"))
+(defun home()
+  (interactive)
+  (dired "~"))
+
+;;; alias end
+
+;; TODO: debian 设置 自动挂载 （udev)
+;; TODO: emacs 里设置 umount
+;; TODO: d -> put to trash instead of rm
+
+;; (defun mount()
+;;   ;; 挂载当前所指向的设备
+;;   (interactive)
+;;   ())
+;; (defun umount()
+;;   ;; 卸载当前所指向的设备
+;;   (interactive)
+;;   ())
+
+(require 'dired-x)
+
+;; DiredOmitMode
+(setq-default dired-omit-files-p t)
+(setq dired-omit-files "^\\.?#\\|^session\\.\\|^\\..*$")
+(add-hook 'dired-mode-hook
+          (lambda()
+            (local-set-key (kbd "h") 'dired-omit-mode)))
+                             
+
 
 ;;; Tired of seeing stale dired buffers?
 ;; Auto refresh buffers
@@ -20,20 +56,20 @@
 
 ;; just press ~ to go home. 
 (add-hook 'ido-setup-hook
- (lambda ()
-   ;; Go straight home
-   (define-key ido-file-completion-map
-     (kbd "~")
-     (lambda ()
-       (interactive)
-       (if (looking-back "/")
-           (insert "~/")
-         (call-interactively 'self-insert-command))))))
+          (lambda ()
+            ;; Go straight home
+            (define-key ido-file-completion-map
+              (kbd "~")
+              (lambda ()
+                (interactive)
+                (if (looking-back "/")
+                    (insert "~/")
+                  (call-interactively 'self-insert-command))))))
 
 ;; Make dired less verbose
-(require 'dired-details)
-(setq-default dired-details-hidden-string "--- ")
-(dired-details-install)
+;; (require 'dired-details)
+;; (setq-default dired-details-hidden-string "--- ")
+;; (dired-details-install)
 
 (require 'dired-filetype-face)
 (provide 'init-dired)
