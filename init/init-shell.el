c641841e65946c9fb09b3056fd68d490cb202ed7
Zeno Zeng <zenoes@qq.com>
2013-08-14 15:50:09 +0800

    using gnome-terminal & zsh instead of eshell

--- init/init-shell.el
+++ init/init-shell.el
@@ -1,2 +1,13 @@
-(defalias 'sh 'eshell)
+(defun my-async-shell-command (cmd)
+  (let* ((buffer-name (format-time-string "*buffer-to-hide-%s*")))
+    (async-shell-command cmd buffer-name nil)
+    (delete-windows-on buffer-name nil)
+    (add-to-list 'ido-ignore-buffers buffer-name)))
+(defun sh()
+  (interactive)
+  (message "%s" default-directory)
+  (my-async-shell-command (concat "gnome-terminal --working-directory="
+                                  (replace-regexp-in-string "~"
+                                                            "/home/zeno"
+                                                            default-directory))))
 (provide 'init-shell)
