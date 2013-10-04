ed1eb7f4f483b7ef1886f17320b7f13ce46c5093
Zeno Zeng <zenoes@qq.com>
2013-10-04 22:33:14 +0800

    add my snippet

--- init/init-snippet.el
+++ init/init-snippet.el
@@ -2,7 +2,7 @@
 (require 'yasnippet)
 (setq yas-snippet-dirs
       '("~/.emacs.d/snippets"
-	"~/.emacs.d/elpa/yasnippet-20130907.1855/snippets"
-	))
+        "~/.emacs.d/elpa/yasnippet-20130907.1855/snippets"
+        ))
 (yas-global-mode 1)
 (provide 'init-snippet)
