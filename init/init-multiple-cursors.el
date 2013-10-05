9ee5752c84b692707d97f3bbc6dc13a7a39b881a
Zeno Zeng <zenoes@qq.com>
2013-10-05 22:42:26 +0800

    add mutiple-cursor

---
+++ init/init-multiple-cursors.el
@@ -0,0 +1,6 @@
+(require 'multiple-cursors)
+(global-set-key (kbd "C-c .") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-c ,") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c >") 'mc/skip-to-next-like-this)
+(global-set-key (kbd "C-c <") 'mc/skip-to-previous-like-this)
+(provide 'init-multiple-cursors)
