48e5c74651447f6a755bab4527fa8fc59be6f363
Zeno Zeng <zenoofzeng@gmail.com>
2014-03-18 19:54:24 +0800

    update keybinding for paredit

--- init/init-paredit.el
+++ init/init-paredit.el
@@ -1,8 +1,12 @@
+(require 'paredit)
+
 (dolist (hook '(clojure-mode-hook
                 emacs-lisp-mode-hook
                 scheme-mode-hook))
   (add-hook hook
             (lambda ()
               (paredit-mode 1))))
+
+(define-key paredit-mode-map (kbd "C-9") 'paredit-wrap-round)
 
 (provide 'init-paredit)
