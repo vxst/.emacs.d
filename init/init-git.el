afdccac8bbb4ec57dcf3e8778575fdc905ac75cc
Zeno Zeng <zenoofzeng@gmail.com>
2014-07-15 17:48:17 +0800

    add usage in init-git

--- init/init-git.el
+++ init/init-git.el
@@ -1,2 +1,12 @@
+;; M-x magit-status to see git status, and in the status buffer:
+;; s to stage files
+;; c c to commit (type in your commit message then C-c C-c to save the message and commit)
+;; b b to switch to a branch
+;; Other handy keys:
+
+;; P P to do a git push
+;; F F to do a git pull
+;; try to press TAB
+
 (defalias 'st 'magit-status)
 (provide 'init-git)
