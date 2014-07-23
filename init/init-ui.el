8de10bac2747e7e9af744ab8ef6ce3b0b861f4f6
Zeno Zeng <zenoofzeng@gmail.com>
2014-07-24 00:21:06 +0800

    use M+ 1mn regular

--- init/init-ui.el
+++ init/init-ui.el
@@ -105,7 +105,7 @@
    ;; If you edit it by hand, you could mess it up, so be careful.
    ;; Your init file should contain only one such instance.
    ;; If there is more than one, they won't work right.
-   '(default ((t (:inherit nil :stipple nil :background "#000" :foreground "#fff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :width normal :foundry "unknown" :family "Source Code Pro"))))
+   '(default ((t (:inherit nil :stipple nil :background "#000" :foreground "#fff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :width normal :foundry "unknown" :family "M+ 1mn regular"))))
    '(emms-lyrics-minibuffer-face ((t (:foreground "green yellow" :height 300))))
    '(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face))))
    '(font-lock-comment-face ((t (:foreground "#999" :height 1.0))))
